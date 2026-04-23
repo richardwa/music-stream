1da9b7d2155dced18fc4afd49ee9e20610b78797
rich.sammus <rich.sammus>
2026-04-22 20:17:38 -0400

    update

--- src/server/conf.ts
+++ src/server/conf.ts
@@ -1,2 +1,4 @@
-import path from "path";
-export const musicFolder = process.env.MUSIC_FOLDER || "/home/rich/music";
+if (process.env.MUSIC_FOLDER == null) {
+  throw new Error("MUSIC_FOLDER not defined");
+}
+export const musicFolder = process.env.MUSIC_FOLDER;
