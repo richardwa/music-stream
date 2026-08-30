03c4ee821910ff1b6a7c0e76c5fde8deed2c368a
dev.trigkey <dev.trigkey>
2026-08-29 21:03:01 -0400

    update

--- run-dev.sh
+++ run-dev.sh
@@ -2,9 +2,4 @@
 
 npm run build
 
-MUSIC_FOLDER=~/music \
-  VIDEO_FOLDER=~/video \
-  YT_CACHE=~/.yt-download/cache \
-  YT_TMP=~/.yt-download/tmp \
-  YT_DOWNLOADED_TXT=~/.yt-download/downloaded.txt \
-  npm run dev
+MUSIC_FOLDER=./sample-music npm run dev
